8a5730f5325ae6a397d8de4b62096f90f435d96a
felipe <profissionalf.andre@gmail.com>
2024-10-09 20:36:08 -0300

    feat: use class validator to validate my data in my dtos

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -1,9 +1,22 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 import { MessageEntity } from '../entities/message-entity';
 
 type CreateRecadoDTO = Pick<MessageEntity, 'text' | 'of' | 'to'>;
 
 export class CreateMessageDTO implements CreateRecadoDTO {
+  @IsString({
+    message: 'the `text` needs a valid text',
+  })
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(255)
   readonly text: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly of: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly to: string;
 }
--- src/messages/dto/update-message.dto.ts
+++ src/messages/dto/update-message.dto.ts
@@ -1,5 +1,38 @@
+import {
+  IsBoolean,
+  IsDate,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import { MessageEntity } from '../entities/message-entity';
 
 type UpdateRecadoDTO = Partial<MessageEntity>;
 
-export class UpdateMessageDTO implements UpdateRecadoDTO {}
+export class UpdateMessageDTO implements UpdateRecadoDTO {
+  @IsDate()
+  @IsOptional()
+  date?: Date;
+
+  @IsString()
+  @IsOptional()
+  readonly of: string;
+
+  @IsString()
+  @IsOptional()
+  readonly to: string;
+
+  @IsOptional()
+  @IsBoolean()
+  read?: boolean;
+
+  @IsString({
+    message: 'the `text` needs a valid text',
+  })
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(255)
+  readonly text: string;
+}
