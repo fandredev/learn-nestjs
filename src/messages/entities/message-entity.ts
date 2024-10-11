e8a73eaba056434932fabd6398c36c936106bcff
felipe <profissionalf.andre@gmail.com>
2024-10-11 13:50:30 -0300

    refactor: Update message-entity relationships to use CASCADE delete and update options

--- src/messages/entities/message-entity.ts
+++ src/messages/entities/message-entity.ts
@@ -18,11 +18,11 @@ export class Message {
   @Column({ type: 'varchar', length: 255 })
   text: string;
 
-  @ManyToOne(() => Person) // Muitos recados podem ser enviados por uma única pessoa
+  @ManyToOne(() => Person, { onDelete: 'CASCADE', onUpdate: 'CASCADE' }) // Muitos recados podem ser enviados por uma única pessoa
   @JoinColumn({ name: 'of' }) // Especifica a coluna 'de' que armazena o id da pessoa que enviou o recado
   of: Person;
 
-  @ManyToOne(() => Person) // Muitos recados podem ser enviados para uma única pessoa
+  @ManyToOne(() => Person, { onDelete: 'CASCADE', onUpdate: 'CASCADE' }) // Muitos recados podem ser enviados para uma única pessoa
   @JoinColumn({ name: 'to' }) // Especifica a coluna 'de' que armazena o id da pessoa que recebe o recado
   to: Person;
 
