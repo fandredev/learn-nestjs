94317558034378725cebcc574ca02ea07c6a2756
felipe <profissionalf.andre@gmail.com>
2024-10-11 11:58:12 -0300

    refactor: Refactor class Message Entity to use TypeORM decorators for column definitions using postgres

--- src/messages/entities/message-entity.ts
+++ src/messages/entities/message-entity.ts
@@ -1,9 +1,35 @@
-export class MessageEntity {
-  // Poderia ser uma interface também
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+// Entidade do banco de dado
+export class Message {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255 })
   text: string;
+
+  @Column({ type: 'varchar', length: 255 })
   of: string;
+
+  @Column({ type: 'varchar', length: 255 })
   to: string;
+
+  @Column({ type: 'boolean', default: false })
   read: boolean;
+
+  @Column({ type: 'date' })
   date: Date;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
 }
