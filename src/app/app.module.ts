69a1db693f51eac72ce2d5cdfcbae18f9e877223
felipe <profissionalf.andre@gmail.com>
2024-10-12 15:20:41 -0300

    chore: Add .env file to hide database configurations
    - Add hapi/joi to validate .env keys

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,36 @@ import { MessagesModule } from 'src/messages/messages.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PersonModule } from 'src/person/person.module';
 import { SimpleMiddleware } from 'src/middlewares/simple.middleware';
+import { ConfigModule } from '@nestjs/config';
 // import { IsAdminGuard } from 'src/common/guards/is-admin.guard';
+
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
     MessagesModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().default(5432),
+        DATABASE_USERNAME: Joi.string().required(),
+        DATABASE_DATABASE: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_AUTOLOADENTITIES: Joi.number().min(0).max(1).default(0),
+        DATABASE_SYNCRONIZE: Joi.number().min(0).max(1).default(0),
+      }),
+    }),
     PersonModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      database: 'udemynest',
-      password: '123456',
-      autoLoadEntities: true, // Carrega entidades sem precisar especificar elas
-      synchronize: true, // Sincroniza com o DB. Não deve ser usado em produção!
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      database: process.env.DATABASE_DATABASE,
+      password: process.env.DATABASE_PASSWORD,
+      autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES), // Carrega entidades sem precisar especificar elas
+      synchronize: Boolean(process.env.DATABASE_SYNCRONIZE), // Sincroniza com o DB. Não deve ser usado em produção!
     }),
   ],
   controllers: [AppController],
