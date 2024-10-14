e64d6392ea875c74096b875f05811ada02863ac3
felipe <profissionalf.andre@gmail.com>
2024-10-14 14:49:14 -0300

    chore: create login service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,8 @@
 import { Global, Module } from '@nestjs/common';
 import { HashProtocolService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcyrpt.service';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Global() // Não preciso importar o AuthModule em outros módulos
 @Module({
@@ -9,7 +11,9 @@ import { BcryptService } from './hashing/bcyrpt.service';
       provide: HashProtocolService,
       useClass: BcryptService,
     },
+    AuthService,
   ],
+  controllers: [AuthController],
   exports: [HashProtocolService],
 })
 export class AuthModule {}
