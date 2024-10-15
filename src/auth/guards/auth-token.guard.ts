681c427c043381980da643b834d2e4e97c11f97d
felipe <profissionalf.andre@gmail.com>
2024-10-14 23:06:22 -0300

    feat: Add AuthTokenGuard to handle JWT authentication
    This commit adds a new file, `AuthTokenGuard`, which is responsible for handling JWT authentication in the application. It implements the `CanActivate` interface from `@nestjs/common` and is used as a guard in the authentication flow.
    
    The `AuthTokenGuard` extracts the JWT token from the request header and verifies it using the `JwtService` and `ConfigService` from `@nestjs/jwt` and `@nestjs/config` respectively. If the token is valid, the payload is stored in the request object for further processing. If the token is missing or invalid, an `UnauthorizedException` is thrown.
    
    This guard is an important part of the authentication process and ensures that only authenticated users can access protected routes.

---
+++ src/auth/guards/auth-token.guard.ts
@@ -0,0 +1,53 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { REQUEST_TOKEN_PAYLOAD_KEY } from '../auth.constants';
+
+@Injectable()
+export class AuthTokenGuard implements CanActivate {
+  private readonly logger = new Logger();
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException('Failed to login - Token not found');
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(
+        token,
+        this.configService.get('jwt'),
+      );
+
+      request[REQUEST_TOKEN_PAYLOAD_KEY] = payload;
+    } catch (error) {
+      this.logger.error(error.message || 'Failed to login');
+      throw new UnauthorizedException(error.message || 'Failed to login');
+    }
+
+    return true;
+  }
+
+  extractTokenFromHeader(request: Request) {
+    const authorization = request.headers?.authorization;
+
+    if (!authorization || typeof authorization !== 'string') {
+      return null;
+    }
+
+    return authorization.split(' ')[1];
+  }
+}
